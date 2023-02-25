248669f3a8aa738e27bb70f76c53d4ae92149a30
Mihail Dragnev <bymykel5@gmail.com>
2023-02-25 12:49:49 +0100

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+  plugins: []
+}
