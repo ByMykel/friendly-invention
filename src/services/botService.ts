203b574d4db742aacd25de8247282312198d9a7c
Mihail Dragnev <bymykel5@gmail.com>
2023-02-26 10:39:30 +0100

    feat: fake backend requests.

--- src/services/botService.ts
+++ src/services/botService.ts
@@ -1,42 +1,72 @@
-import axios from 'axios'
-import { BASE_URL } from '../utils/constants'
+import { LoremIpsum } from 'lorem-ipsum'
+
+// Fake bot messages
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4
+  }
+})
+
+// Fake delay to simulate a real API call
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+// Simulate 401 error
+const unauthorizedError = new Error('Unauthorized')
+// @ts-ignore
+unauthorizedError.response = {
+  status: 401
+}
 
 export const login = async (user: string, password: string) => {
-  const response = await axios.post(`${BASE_URL}/login`, {
-    user,
-    password,
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
+  await sleep(100)
 
-  return response.data
+  if (user === 'admin' && password === 'admin') {
+    return {
+      session_id: 'token_123'
+    }
+  }
+
+  throw unauthorizedError
 }
 
 export const getWelcomeMessage = async () => {
-  const response = await axios.get(`${BASE_URL}/getWelcomeMessage`, {
-    headers: {
-      Authorization: `Bearer ${sessionStorage.getItem('token')}`,
-      'Content-Type': 'application/json'
-    }
-  })
+  await sleep(200)
 
-  return response.data.response
+  if (!sessionStorage.getItem('token')) {
+    throw unauthorizedError
+  }
+
+  return [
+    { type: 'text', text: 'Welcome to the bot!' },
+    {
+      type: 'image',
+      url: 'https://i.pinimg.com/originals/4e/6f/9a/4e6f9a7e295ce34133d55a3c63394304.jpg'
+    }
+  ]
 }
 
 export const sendMessage = async (userMessage: string) => {
-  const response = await axios.post(
-    `${BASE_URL}/sendMessage`,
-    {
-      text: userMessage
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${sessionStorage.getItem('token')}`,
-        'Content-Type': 'application/json'
-      }
-    }
-  )
+  await sleep(400)
 
-  return response.data.response
+  if (!sessionStorage.getItem('token')) {
+    throw unauthorizedError
+  }
+
+  if (userMessage === 'image') {
+    return [{ type: 'image', url: 'https://picsum.photos/200' }]
+  }
+
+  const randomNumberOfMessages = Math.floor(Math.random() * 5) + 1
+
+  return Array.from({ length: randomNumberOfMessages }, () => {
+    return {
+      type: 'text',
+      text: lorem.generateSentences(1)
+    }
+  })
 }
